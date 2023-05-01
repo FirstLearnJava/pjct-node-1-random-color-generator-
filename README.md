fa78a2defa7d706548842c6845ad4aa053e14535
MattHere <matthiasfloimayr@gmail.com>
2023-05-01 18:52:39 +0200

    first endversion

---
+++ README.md
@@ -0,0 +1,26 @@
+# Node.js Random Color Generator
+
+Generate random colors on the command line,
+which match the following example-pattern:
+
+#
+
+#
+
+#
+
+#
+
+# ##### #edff28
+
+#
+
+#
+
+#
+
+#
+
+```bash
+TODO: run   node index.js
+```
---
+++ index.js
@@ -0,0 +1,29 @@
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+
+// The first value the user inserts after typing in node index.js into the console is going to be assigned to hue.
+const hue = process.argv[2];
+
+// The second value the user inserts after typing in node index.js into the console is going to be assigned to luminosity.
+const luminosity = process.argv[3];
+
+const actualRandomColor = randomColor({
+  hue: hue,
+  luminosity: luminosity,
+});
+
+console.log(chalk.hex(actualRandomColor)('###############################'));
+console.log(chalk.hex(actualRandomColor)('###############################'));
+console.log(chalk.hex(actualRandomColor)('###############################'));
+console.log(
+  chalk.hex(actualRandomColor)(`#####       ${actualRandomColor}       #####`),
+);
+console.log(
+  chalk.hex(actualRandomColor)(`#####       ${actualRandomColor}       #####`),
+);
+console.log(
+  chalk.hex(actualRandomColor)(`#####       ${actualRandomColor}       #####`),
+);
+console.log(chalk.hex(actualRandomColor)('###############################'));
+console.log(chalk.hex(actualRandomColor)('###############################'));
+console.log(chalk.hex(actualRandomColor)('###############################'));
